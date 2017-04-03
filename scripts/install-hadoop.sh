8b7557fafde96586e36d8b0da56bc51e235e9028
Your Name <you@example.com>
2017-04-03 22:11:47 +0700

    update hdfs

--- scripts/install-hadoop.sh
+++ scripts/install-hadoop.sh
@@ -8,7 +8,7 @@ install_dir=/usr/local
 
 cd $install_dir
 wget "http://apache.repo.unpas.ac.id/hadoop/common/hadoop-2.7.2/hadoop-2.7.2.tar.gz"
-tar -xvzf hadoop-2.7.2.tar.gz hadoop
+tar -xvzf hadoop-2.7.2.tar.gz -C hadoop 
 
 echo "export HADOOP_HOME=/usr/local/hadoop" >> ~/.bashrc
 echo "export PATH=$PATH:$HADOOP_HOME/bin/" >> ~/.bashrc
