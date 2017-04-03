71c188f98f1216d18d81f8925da7ac43b84d5999
Your Name <you@example.com>
2017-04-03 22:53:18 +0700

    update hdfs

--- scripts/install-defaults.sh
+++ scripts/install-defaults.sh
@@ -5,7 +5,7 @@ echo " install default utils"
 echo "============================="
 
 apt-get update
-apt-get install curl wget openssh-client -y
+apt-get install curl wget openssh-client  openssh-server -y
 
 ssh-keygen -t dsa -P '' -f ~/.ssh/id_dsa
 cat ~/.ssh/id_dsa.pub >> ~/.ssh/authorized_keys
