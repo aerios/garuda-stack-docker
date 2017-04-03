cc78cb83bc2b9cd2fa089529fc512ec292511fff
Your Name <you@example.com>
2017-04-03 23:03:30 +0700

    update hdfs

--- scripts/install-defaults.sh
+++ scripts/install-defaults.sh
@@ -10,3 +10,5 @@ apt-get install curl wget openssh-client  openssh-server -y
 ssh-keygen -t dsa -P '' -f ~/.ssh/id_dsa
 cat ~/.ssh/id_dsa.pub >> ~/.ssh/authorized_keys
 
+mkdir /var/run/sshd
+chmod 0755 /var/run/sshd
